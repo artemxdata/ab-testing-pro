f37743b506c5fbef9b7995ee394ae61b9cf52085
artemxdata <artemfromspace@outlook.com>
2025-07-28 10:25:40 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-// src/index.js - Application Entry Point
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
-
-// Service Worker для PWA
-import { registerSW } from './serviceWorker';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// Регистрируем Service Worker для PWA функциональности
-registerSW();
-
-// Web Vitals для мониторинга производительности
-import('./webVitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-  getCLS(console.log);
-  getFID(console.log);
-  getFCP(console.log);
-  getLCP(console.log);
-  getTTFB(console.log);
-});
